9a57342016dfa3470216a37edcc59e5f1d6bc761
yunus.demirpolat <yunus.demirpolat@KZL-YZLMORT-B14>
2019-11-01 18:24:59 +0300

    Add project files.

---
+++ Yazaralemi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yazaralemi.Startup))]
+namespace Yazaralemi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
