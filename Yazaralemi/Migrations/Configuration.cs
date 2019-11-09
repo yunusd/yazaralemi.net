27b4b98a4f66f8ed1bd8942da27d780929ab0c6c
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2019-11-09 14:42:52 +0300

    change turkish chars to english

--- Yazaralemi/Migrations/Configuration.cs
+++ Yazaralemi/Migrations/Configuration.cs
@@ -21,7 +21,7 @@ namespace Yazaralemi.Migrations
             string email = "yunusdemirpolatt@gmail.com";
             string password = "123456";
 
-            #region admin rolünü ve kullanýcýsýný oluþtur
+            #region admin rolünü ve kullanicisi olustur
             if (!context.Roles.Any(r => r.Name == "admin"))
             {
                 var store = new RoleStore<IdentityRole>(context);
@@ -40,20 +40,20 @@ namespace Yazaralemi.Migrations
                 manager.Create(user, password);
                 manager.AddToRole(user.Id, "admin");
 
-                //Oluþturulan bu kullanýcýya ait yazýlar ekler
+                //Olusturulan bu kullaniciya ait yazilar ekler
                 #region Kategoriler ve Yazýlar
                 if (!context.Categories.Any())
                 {
                     Category cat1 = new Category
                     {
-                        CategoryName = "Gezi Yazýlarý"
+                        CategoryName = "Gezi Yazilari"
                     };
 
                     cat1.Posts = new List<Post>();
 
                     cat1.Posts.Add(new Post
                     {
-                        Title = "Gezi Yazýsý 1",
+                        Title = "Gezi Yazisi 1",
                         AuthorId = user.Id,
                         Content = @"<p>Maecenas finibus vel justo non mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent placerat est ac risus faucibus, vel eleifend felis luctus. Aliquam luctus sollicitudin quam, quis auctor est facilisis ac. Proin et molestie ipsum. Donec eu rutrum libero, sed efficitur nisl. Vestibulum feugiat, lectus ac volutpat sollicitudin, turpis sapien maximus ipsum, sit amet ultrices erat lacus non neque. Aliquam a ipsum at leo mattis volutpat.</p><p>        Maecenas et erat nisi. Nam pellentesque leo quam, eu malesuada nulla commodo ac. Aliquam aliquam, enim tincidunt varius laoreet, tellus orci egestas tortor, id vehicula lorem turpis ac turpis. Sed non odio mauris. Sed neque erat, fermentum aliquet semper sed, elementum quis eros. Cras sed felis neque. Cras dignissim lacus purus, eget hendrerit lacus auctor ut.</p>",
                         CreatedAt = DateTime.Now
@@ -61,7 +61,7 @@ namespace Yazaralemi.Migrations
 
                     cat1.Posts.Add(new Post
                     {
-                        Title = "Gezi Yazýsý 2",
+                        Title = "Gezi Yazisi 2",
                         AuthorId = user.Id,
                         Content = @"<p>Maecenas finibus vel justo non mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent placerat est ac risus faucibus, vel eleifend felis luctus. Aliquam luctus sollicitudin quam, quis auctor est facilisis ac. Proin et molestie ipsum. Donec eu rutrum libero, sed efficitur nisl. Vestibulum feugiat, lectus ac volutpat sollicitudin, turpis sapien maximus ipsum, sit amet ultrices erat lacus non neque. Aliquam a ipsum at leo mattis volutpat.</p><p>        Maecenas et erat nisi. Nam pellentesque leo quam, eu malesuada nulla commodo ac. Aliquam aliquam, enim tincidunt varius laoreet, tellus orci egestas tortor, id vehicula lorem turpis ac turpis. Sed non odio mauris. Sed neque erat, fermentum aliquet semper sed, elementum quis eros. Cras sed felis neque. Cras dignissim lacus purus, eget hendrerit lacus auctor ut.</p>",
                         CreatedAt = DateTime.Now
@@ -70,14 +70,14 @@ namespace Yazaralemi.Migrations
 
                     Category cat2 = new Category
                     {
-                        CategoryName = "Ýþ Yazýlarý"
+                        CategoryName = "Is Yazilari"
                     };
 
                     cat2.Posts = new List<Post>();
 
                     cat2.Posts.Add(new Post
                     {
-                        Title = "Ýþ Yazýsý 1",
+                        Title = "Is Yazisi 1",
                         AuthorId = user.Id,
                         Content = @"<p>Maecenas finibus vel justo non mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent placerat est ac risus faucibus, vel eleifend felis luctus. Aliquam luctus sollicitudin quam, quis auctor est facilisis ac. Proin et molestie ipsum. Donec eu rutrum libero, sed efficitur nisl. Vestibulum feugiat, lectus ac volutpat sollicitudin, turpis sapien maximus ipsum, sit amet ultrices erat lacus non neque. Aliquam a ipsum at leo mattis volutpat.</p><p>        Maecenas et erat nisi. Nam pellentesque leo quam, eu malesuada nulla commodo ac. Aliquam aliquam, enim tincidunt varius laoreet, tellus orci egestas tortor, id vehicula lorem turpis ac turpis. Sed non odio mauris. Sed neque erat, fermentum aliquet semper sed, elementum quis eros. Cras sed felis neque. Cras dignissim lacus purus, eget hendrerit lacus auctor ut.</p>",
                         CreatedAt = DateTime.Now
@@ -86,7 +86,7 @@ namespace Yazaralemi.Migrations
 
                     cat2.Posts.Add(new Post
                     {
-                        Title = "Ýþ Yazýsý 2",
+                        Title = "Is Yazisi 2",
                         AuthorId = user.Id,
                         Content = @"<p>Maecenas finibus vel justo non mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent placerat est ac risus faucibus, vel eleifend felis luctus. Aliquam luctus sollicitudin quam, quis auctor est facilisis ac. Proin et molestie ipsum. Donec eu rutrum libero, sed efficitur nisl. Vestibulum feugiat, lectus ac volutpat sollicitudin, turpis sapien maximus ipsum, sit amet ultrices erat lacus non neque. Aliquam a ipsum at leo mattis volutpat.</p><p>        Maecenas et erat nisi. Nam pellentesque leo quam, eu malesuada nulla commodo ac. Aliquam aliquam, enim tincidunt varius laoreet, tellus orci egestas tortor, id vehicula lorem turpis ac turpis. Sed non odio mauris. Sed neque erat, fermentum aliquet semper sed, elementum quis eros. Cras sed felis neque. Cras dignissim lacus purus, eget hendrerit lacus auctor ut.</p>",
                         CreatedAt = DateTime.Now
@@ -99,8 +99,8 @@ namespace Yazaralemi.Migrations
             }
             #endregion
 
-            #region Admin kullanýcýsýna 77 yeni yazý ekle
-            string catName = "Diðer";
+            #region Admin kullanicisina 77 yeni yazi ekle
+            string catName = "Diger";
             if (!context.Categories.Any(x => x.CategoryName == catName))
             {
                 ApplicationUser admin = context.Users.Where(x => x.UserName == email).FirstOrDefault();
@@ -117,7 +117,7 @@ namespace Yazaralemi.Migrations
                         {
                             diger.Posts.Add(new Post
                             {
-                                Title = "Gezi Yazýsý " + i,
+                                Title = "Gezi Yazisi " + i,
                                 AuthorId = admin.Id,
                                 Content = @"<p>Maecenas finibus vel justo non mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent placerat est ac risus faucibus, vel eleifend felis luctus. Aliquam luctus sollicitudin quam, quis auctor est facilisis ac. Proin et molestie ipsum. Donec eu rutrum libero, sed efficitur nisl. Vestibulum feugiat, lectus ac volutpat sollicitudin, turpis sapien maximus ipsum, sit amet ultrices erat lacus non neque. Aliquam a ipsum at leo mattis volutpat.</p><p>        Maecenas et erat nisi. Nam pellentesque leo quam, eu malesuada nulla commodo ac. Aliquam aliquam, enim tincidunt varius laoreet, tellus orci egestas tortor, id vehicula lorem turpis ac turpis. Sed non odio mauris. Sed neque erat, fermentum aliquet semper sed, elementum quis eros. Cras sed felis neque. Cras dignissim lacus purus, eget hendrerit lacus auctor ut.</p>",
                                 CreatedAt = DateTime.Now.AddMinutes(i)
