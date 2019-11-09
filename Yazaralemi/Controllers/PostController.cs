3b81767e1c13eb42c0396a98250eeb1d8d439fbe
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2019-11-09 18:00:30 +0300

    add post detail page and comment section

---
+++ Yazaralemi/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Yazaralemi.Controllers
+{
+    public class PostController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index(int cid, int pid)
+        {
+            var category = ctx.Categories.Find(cid);
+            var post = ctx.Posts.Find(pid);
+            if(category == null || post == null) return PartialView("_NotFound");
+
+            return View(post);
+        }
+    }
+}
